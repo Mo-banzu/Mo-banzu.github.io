33792fa629dcf3db4e361cf48fd7acad107e2c5e
Andre Pradana <100468904+Mo-banzu@users.noreply.github.com>
2022-02-27 00:22:11 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".nav-menu");
+
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+
+    // add menu icon
+    toggle.innerHTML = `<i class="fas fa-bars">`;
+  } else {
+    menu.classList.add("active");
+
+    // add x (close menu) icon
+    toggle.innerHTML = `<i class="fas fa-times"></i>`;
+  }
+}
+
+toggle.addEventListener("click", toggleMenu, false);
