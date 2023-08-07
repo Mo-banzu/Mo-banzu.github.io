6a170321487a3a1f4c909959643bc034915645fa
Andre Pradana <mo.banzu@gmail.com>
2023-08-07 08:59:06 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,13 +4,9 @@ const menu = document.querySelector(".nav-menu");
 function toggleMenu() {
   if (menu.classList.contains("active")) {
     menu.classList.remove("active");
-
-    // add menu icon
     toggle.innerHTML = `<i class="fas fa-bars">`;
   } else {
     menu.classList.add("active");
-
-    // add x (close menu) icon
     toggle.innerHTML = `<i class="fas fa-times"></i>`;
   }
 }
